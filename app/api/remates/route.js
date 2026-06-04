b7efc4c2465fa1e7043a4034ad397fcd7e928ede
Melanye11 <me.pache@duocuc.cl>
2026-06-04 01:18:03 -0400

    Refactor GET to use getRematesActivos service
    Refactor GET function to use getRematesActivos service instead of direct API call.

--- app/api/remates/route.js
+++ app/api/remates/route.js
@@ -1,21 +1,20 @@
 import { NextResponse } from 'next/server';
+import { getRematesActivos} from '@/services/tgrService;
 
 export async function GET() {
-  const url = "https://remates.tgr.cl/v1/getListaRematesActivos";
   try {
-    const respuesta = await fetch(url, {
-      method: "GET",
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-        "Accept": "application/json"
-      },
-      next: { revalidate: 3600 } 
-    });
+    const datosLimpios = await getRematesActivos();
+    
+      return NextResponse.json({
+        sucess: true,
+        count: datosLimpios.length,
+        data: datosLimpios
+      }, {status: 200 });
 
-    if (!respuesta.ok) return NextResponse.json({ error: "Error API" }, { status: respuesta.status });
-    const datos = await respuesta.json();
-    return NextResponse.json(datos.data || []);
   } catch (error) {
-    return NextResponse.json({ error: "Error interno" }, { status: 500 });
+    return NextResponse.json({ 
+      success: false,
+      error: " Error al intentar obtener los remates "
+    }, { satus: 500 });
   }
 }
